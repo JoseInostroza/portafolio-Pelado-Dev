5ee919c0fb366eef166b676ec64da113369d3c3d
JoseInostroza <87823768+JoseInostroza@users.noreply.github.com>
2023-01-26 13:29:50 -0300

    bases cards hechas

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,35 @@ module.exports = {
   theme: {
     extend: {
       colors:{
-        principal: '#CFF5E7',
-        secundario:'#A0E4CB',
-        terciario: '#59C1BD',
-        Text:'#0D4C92',
+        principal: '#1B262C',
+        secundario:'#0F4C75',
+        terciario: '#3282B8',
+        text:'#BBE1FA'
+      },
+      textColor:{
+        principal: '#1B262C',
+        secundario:'#0F4C75',
+        terciario: '#3282B8',
+        text:'#BBE1FA'
+      },
+      backgroundImage : {
+        'sanfrancisco': "url(../img/sanFrancisco.jpg)",
+        'sanfranciscoD': "url(../img/sanFranciscoDesktop.jpg)",
+        'bali': "url(../img/bali.jpg)",
+        'chicago': "url(../img/chicago.jpg)",
+        'europe': "url(../img/europe.jpg)",
+        'iceland': "url(../img/iceland.jpg)",
+        'LA': "url(../img/LA.jpg)",
+        'miami': "url(../img/miami.jpg)",
+        'NY': "url(../img/new_york.jpg)",
+        'norway': "url(../img/norway.jpg)",
+        'seattle': "url(../img/seattle.jpg)",
+        'switzerland': "url(../img/switzerland.jpg)",
+        'sydney': "url(../img/sydney.jpg)",
+        'yosemite': "url(../img/yosemite.jpg)"
+      },
+      fontFamily:{
+        Noto: ['Noto Serif Display', 'serif']
       }
     },
   },
